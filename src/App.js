b6af461b91bf325110a79773b19a53f9e6833ea8
Pratham <temp@Mac.lan>
2025-04-01 13:32:14 +0530

    Added basic grid and click functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const gridSize = 4;
+const clickSymbol = "O";
+
+function GridRow({ size }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        columnGap: "10px",
+        marginTop: "5px",
+        marginBottom: "5px",
+      }}
+      className="grid-row"
+    >
+      {Array.from({ length: size }).map((_, index) => (
+        <div key={index} className="grid-cell">
+          <button
+            style={{
+              width: 100,
+              height: 100,
+              backgroundColor: "#f0f0f0",
+              border: "1px solid black",
+              fontSize: "34px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            className="grid-cell-button"
+            onClick={(event) => {
+              event.target.innerText = clickSymbol;
+            }}
+          >
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      className="App"
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        className="App"
+      >
+        {Array.from({ length: gridSize }).map((_, index) => (
+          <GridRow key={index} size={gridSize} />
+        ))}
+      </div>
     </div>
   );
 }
